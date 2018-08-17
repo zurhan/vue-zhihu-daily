e931a3224631053797032013f61d0f43dc7a6224
huangzh <huangzehang@chinamobile.com>
2018-08-17 18:54:19 +0800

    add header

---
+++ src/components/DailyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="zhihuheader">
+    <header class="header">
+      <nav class="inner">
+        <router-link to="/">知乎日报</router-link>
+        <router-link to="/daily">今日热闻</router-link>
+        <router-link to="/theme">专题</router-link>
+        <a class="github" href="https://github.com/zurhan/vue-zhihu-daily" target="_blank">Built with Vue.js by Zurhan</a>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.header {
+  background-color: #0022ff
+}
+</style>
---
+++ src/components/DailyList.vue
@@ -0,0 +1,48 @@
+<template>
+<div id="DailyList">
+  <div class="news-list">
+  <div class="box">
+    <a>gasdfa</a>
+  </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      stories: []
+    }
+  }
+}
+</script>
+
+<style>
+.box {
+  max-width: 30px;
+  max-height: 60px;
+}
+.box .a {
+  text-align: center
+}
+.news-list, .news-list-nav {
+    background-color: #fff;
+    border-radius: 2px
+}
+.news-list {
+    position: absolute;
+    margin: 30px 0;
+    width: 100%;
+    -webkit-transition: all .5s cubic-bezier(.55, 0, .1, 1);
+    -o-transition: all .5s cubic-bezier(.55, 0, .1, 1);
+    transition: all .5s cubic-bezier(.55, 0, .1, 1)
+}
+.news-item {
+    background-color: #fff;
+    padding: 20px 30px 20px 80px;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    line-height: 60px
+}
+</style>
